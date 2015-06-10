967f00350d519d4ab13b84607e7ad0914c193fb0
vinicius emanuel <vinicemanuel@vinic-MacBook.local>
2015-06-10 16:05:33 -0300

    controlador de login, faltava criar

---
+++ Midas/MILoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  MILoginViewController.h
+//  Midas
+//
+//  Created by vinicius emanuel on 10/06/15.
+//  Copyright (c) 2015 rfccg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MILoginViewController : UIViewController
+
+@end
